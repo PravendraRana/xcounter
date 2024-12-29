c8813baa422d7f51a148ec08a91f02b2637a8396
PravendraRana <pravendra56rana@gmail.com>
2024-12-29 21:06:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import CounterApp from './CounterApp'
+
+function App() {
+  return (
+    <>
+      <CounterApp/>
+    </>
+  )
+}
+
+export default App
